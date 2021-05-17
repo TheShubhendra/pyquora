8c5e42aee9188dc0f37341ff51900bb5a9090520
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-05-17 14:41:06 +0530

    Add answer class

---
+++ quora/answer.py
@@ -0,0 +1,18 @@
+"""Answer class to represent a Quora answer."""
+from quora.content import Content
+from quora.question import Question
+
+class Answer:
+    def __init__(self, data_dict):
+        self.id = data_dict.get("id")
+        self.aid = data_dict.get("aid")
+        self.isPinned = data_dict.get("isPinned")
+        self.question = Question(data_dict.get("question"))
+        self.url = "https://www.quora.com"+data_dict.get("permaUrl")
+        self.content = Content(data_dict.get("content"))
+        self.creationTime = data_dict.get("creationTime")
+        self.updatedTime = data_dict.get("updatedTime")
+        self.author = data_dict.get("author")
+
+    def __str__(self):
+        return self.content.__str__()
