4368fdad89d64171026a4ebc1de568187a73734a
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-05-17 14:41:30 +0530

    Update Question class

--- quora/question.py
+++ quora/question.py
@@ -1,9 +1,14 @@
 """Question Class for quora question."""
 
+from quora.content import Content
+
 
 class Question:
-    def __init__(self, id, qid, url, title):
-        self.id = (id,)
-        self.qid = (qid,)
-        self.url = url
-        self.title = title
+    def __init__(self, data_dict):
+        self.id = data_dict.get("id")
+        self.qid = data_dict.get("qid")
+        self.url = "https://www.quora.com"+data_dict.get("url")
+        self.title = Content(data_dict.get("title"))
+
+    def __str__(self):
+        return self.title.__str__()
