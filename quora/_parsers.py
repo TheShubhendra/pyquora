858bc2ab7f1818bea182a6411789d15a8dfc6db6
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-05-17 14:40:30 +0530

    Add funtions to parse data

---
+++ quora/_parsers.py
@@ -0,0 +1,22 @@
+import json
+import re
+
+from quora.answer import Answer
+
+
+def parse_page(html_data):
+    try:
+        data = re.findall(
+            r'window\.ansFrontendGlobals\.data\.inlineQueryResults\.results\[".*?"\] = ("{.*}");',
+            html_data,
+        )[-1]
+        data = json.loads(json.loads(data))
+    except Exception:
+        raise ProfileNotFoundError("No profile found with this username.")
+    return data["data"]["user"]
+
+
+def parse_answers(json_data):
+    answers = json_data['recentPublicAndPinnedAnswersConnection']['edges']
+    return [Answer(ans['node']) for ans in answers]
+ 
