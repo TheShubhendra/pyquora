c1d7e3a54b0fb5a7bc2bd5cb266d76c104cebc86
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-07-24 09:25:34 +0530

    Update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def requirements():
 setup(
     name="pyquora",
     packages=find_packages(),
-    version="0.1.1",
+    version="0.2.0",
     license="MIT",
     description="""Fetch profiles and data from Quora.""",
     author="Shubhendra Kushwaha",
