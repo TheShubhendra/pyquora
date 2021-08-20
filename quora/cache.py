8b91aa8e29a9fb0c07e1b9b4e1a013778050c322
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-08-20 09:09:57 +0530

    Minor fixes

--- quora/cache.py
+++ quora/cache.py
@@ -7,14 +7,14 @@ def cache(func):
     async def _wrapper(user, *args, **kwargs):
         key = f"pyquora_{func.__name__}_{user.username}"
         if user._cache is not None:
-            value = await user._cache.get(key)
+            value = user._cache.get(key)
             if value is not None:
                 logger.info(f"Using cache from {key}")
                 return value
         res = await func(user, *args, **kwargs)
         if user._cache is not None:
-            user.logger.info(f"Storing cache into {key}")
-            await user._cache.set(key, res)
+            logger.info(f"Storing cache into {key}")
+            user._cache.set(key, res)
         return res
 
     return _wrapper
