67aca6914bc739e325c5b7cd2f5c38d1bb3a4385
Saurabh Vishwakarma <sauravvishwakarma030@gmail.com>
2023-04-24 12:12:10 +0530

    Updated workflow

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def requirements():
 setup(
     name="pyquora",
     packages=find_packages(),
-    version="0.4.1",
+    version="0.4.2",
     license="MIT",
     description="""Fetch profiles and data from Quora.""",
     author="Shubhendra Kushwaha",
